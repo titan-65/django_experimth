f1deadfb505f520ee802e278d76a981875b4c93f
Vantol Bennett <vvantol2000@icloud.com>
2020-12-17 00:26:32 -0500

    Pages App Addition
    Pages app created and connected with the template.

--- exmaths_porject/settings.py
+++ exmaths_porject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # Local
     'users.apps.UsersConfig',
+    'pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'exmaths_porject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ pages/tests.py
@@ -0,0 +1,20 @@
+from django.test import SimpleTestCase
+from django.urls import reverse
+
+# Create your tests here.
+
+class HomePageTest(SimpleTestCase):
+
+    def setUp(self):
+        url = reverse('index')
+        self.response = self.client.get(url)
+
+    def test_home_page_status(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_url_name(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_template(self):
+        self.assertTemplateUsed(self.response, 'home.html')
+        
